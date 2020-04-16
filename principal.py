df5fbb02c8108496438a4d5903576bc7c7ec53ce
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-04-16 17:11:30 +0200

    principal.py working version commit before work

--- principal.py
+++ principal.py
@@ -1,5 +1,9 @@
 import os
-import analysis.input_data as a_id
+import msvcrt
+import random
+import analysis.input_data as a_id # import source data
+
+
 class Cell:
     
     #class variable
@@ -32,10 +36,13 @@ class Cell:
         
     def available_paths(GRID):
         valid_cells={}
+        valid_cells_list=[]
         for key, value in GRID.items():
             if value == "path":
                 valid_cells[key] = value
-        return valid_cells
+                valid_cells_list.append(key)
+                
+        return valid_cells, valid_cells_list
                
     def __repr__(self):
         return "Ma cellule se trouve à la postion: {}.  Est de type : {}."\
@@ -48,12 +55,22 @@ class MacGyver:
         self.image = "image"
         self.collected_objects =[]
       
+    # def check_cells(self, valid_cells):
+        # if valid_cells[self._n_position]: 
+            # self.position = self._n_position
+            # print(self.position,  "OK") 
+            # continue     
     
     def mac_moves(self, valid_cells):
         
         play = True
         while play:
-            if keyboard.is_pressed("right"):
+        
+            arrow_getch= ord(msvcrt.getch())
+            arrow_getche = ord(msvcrt.getche())
+            
+            # move right
+            if arrow_getch == 224 and arrow_getche == 77:
                 self._n_position = (self.position[0]+1, self.position[1]+0)
                 try:
                     if valid_cells[self._n_position]: 
@@ -62,16 +79,21 @@ class MacGyver:
                         continue                        
                 except:
                     print(self.position,  "MUR")
-                    break 
-            elif keyboard.is_pressed("down"):
+                    continue 
+                    
+            # move down
+            elif arrow_getch == 224 and arrow_getche == 80:
                 self._n_position = (self.position[0]+0, self.position[1]-1)
                 try:
                     if valid_cells[self._n_position]: 
                         self.position = self._n_position
-                        print(self.position,  "OK")    
+                        print(self.position,  "OK") 
+                        continue                        
                 except:
                     print(self.position,  "MUR")
-            elif keyboard.is_pressed("left"):
+                    continue
+            # move left        
+            elif arrow_getch == 224 and arrow_getche == 75:
                 self._n_position = (self.position[0]-1, self.position[1]+ 0)
                 try:
                     if valid_cells[self._n_position]: 
@@ -79,7 +101,9 @@ class MacGyver:
                         print(self.position,  "OK")    
                 except:
                     print(self.position,  "MUR")
-            elif keyboard.is_pressed("up"):
+                    
+            # move up 
+            elif arrow_getch == 224 and arrow_getche == 72:
                 self._n_position = (self.position[0]+0, self.position[1]+ 1)
                 try:
                     if valid_cells[self._n_position]: 
@@ -87,12 +111,10 @@ class MacGyver:
                         print(self.position,  "OK")    
                 except:
                     print(self.position,  "MUR")
+            
+            else:
+                play = False
 
-
-    
-    # def __add__(self, other):
-        # n_position = self.position + other.position
-        # return MacGyver(n_position)
 
 class Guard: 
     
@@ -103,23 +125,40 @@ class Guard:
 
 class Objects: 
     
-    def __init__(self, position):
-        self.position = position 
-        self.object_type = object_type 
-        self.image = image
+    OBJECTS_LIST =["Needle","Plastic tube","Ether"]
+    OBJECTS_DICT={}
+    
+    def __init__(self, name, position):
+        self.position = position
+        self.object_name = name 
+        self.image = "image"
 
-    def random_position(self):
-        #random selection
-        pass
-
+    
+    # def random_position(self, valid_cells_list):
+        # rand_numb = random.randint(0,len(valid_cells_list) - 1)
+        # item = valid_cells_list[rand_numb]
+        # return item
+    
+    @classmethod
+    def initialize_objects(cls,valid_cells_list):
+        for name in cls.OBJECTS_LIST:
+            rand_numb = random.randint(0,len(valid_cells_list) - 1)
+            position = valid_cells_list[rand_numb]
+            lab_object = Objects( name, position)
+            cls.OBJECTS_DICT[position] = name
+            print(lab_object)
+    
+    def __repr__(self):
+        return "Mon objet : {} se trouve à la postion: {}."\
+        .format(self.object_name, self.position)
 
 if __name__ == "__main__":
     content = a_id.get_source('labyrinthe.txt')
     cell = Cell.initialize_cells(content)
-    valid_cells = Cell.available_paths(Cell.GRID)
-    # print (valid_cells)
-    mac = MacGyver((0,7))
-    MacGyver.mac_moves(mac,valid_cells)
+    valid_cells,valid_cells_list = Cell.available_paths(Cell.GRID)
+    Objects.initialize_objects(valid_cells_list)
+    # mac = MacGyver((0,7))
+    # MacGyver.mac_moves(mac,valid_cells)
     #print (nouv_pos)
     # print(cell.position,cell.cell_type,cell.cell_validity)
     # print(Cell.CELLS[(14, 14)])
