606e0ae22011a6cd4408319da36dc5217c2f18da
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-04-17 19:58:56 +0200

    started function on labyrinthe display

--- principal.py
+++ principal.py
@@ -1,13 +1,17 @@
 import os
 import msvcrt
 import random
+import logging as lg
+
 import analysis.input_data as a_id # import source data
 
+lg.basicConfig(level=lg.DEBUG)
 
 class Cell:
     
     #class variable
-    GRID = {}
+    GRID_DICT = {}
+    GRID_LIST = []
     GRID_DIM = 15
     
     def __init__(self,position,cell_type):
@@ -17,28 +21,34 @@ class Cell:
         
     @classmethod
     def initialize_cells(cls,content):
-        caracters_list=[]
         for elt in content:
-            if elt == "-":
-                caracters_list.append("wall")
-            elif elt == "+":
-                caracters_list.append("path")
-            else:
-                pass
+            try:
+                elt =str(elt)
+                if elt == "0":
+                    cls.GRID_LIST.append("wall")
+                elif elt == "1":
+                    cls.GRID_LIST.append("path")
+                elif elt == "2":
+                    cls.GRID_LIST.append("start")
+                elif elt == "3":
+                    cls.GRID_LIST.append("end")
+                else:
+                    pass
+            except:
+                lg.critical ("Str conversion not done")
         
-        for i,elt in enumerate(caracters_list):
+        for i,elt in enumerate(cls.GRID_LIST):
             x = i % cls.GRID_DIM
             y = i // cls.GRID_DIM
             cell = Cell((x,y),elt)
-            cls.GRID[(x,y)] = elt
+            cls.GRID_DICT[(x,y)] = elt
+
         
-        return cell
-        
-    def available_paths(GRID):
+    def available_paths(GRID_DICT):
         valid_cells={}
         valid_cells_list=[]
-        for key, value in GRID.items():
-            if value == "path":
+        for key, value in GRID_DICT.items():
+            if value != "wall":
                 valid_cells[key] = value
                 valid_cells_list.append(key)
                 
@@ -65,7 +75,6 @@ class Objects:
             position = valid_cells_list[rand_numb]
             lab_object = Objects( name, position)
             cls.OBJECTS_DICT[position] = name
-            print(lab_object)
     
     def __repr__(self):
         return "Mon objet : {} se trouve à la postion: {}."\
@@ -73,17 +82,25 @@ class Objects:
 
 
 class MacGyver: 
+
     
-    def __init__(self,position):
-        self.position = position
+    def __init__(self):
+        self.position = (0,0)
+        self.name = "Mac Gyver"
         self.image = "image"
         self.collected_objects =[]
+
     
+    def initial_position(self,valid_cells):
+        for key, value in valid_cells.items():
+            if value == "start":
+                self.position = key
+
+
     def check_position(self,valid_cells):
         try:
             if valid_cells[self._n_position]: 
                 self.position = self._n_position
-                print(self.position,  "OK")
         except:
             print(self.position,  "MUR")
          
@@ -96,8 +113,13 @@ class MacGyver:
                 del objects_dicts[self._n_position]
         except :
             pass
+
+    def show(self,GRID_DIM, GRID_LIST,objects_dicts):
+        self.position[0]
+        position_index = ((self.position[1]) * Cell.GRID_DIM) + self.position[0]
+        print (position_index)
     
-    def mac_moves(self,valid_cells,objects_dicts):
+    def moves(self,valid_cells,objects_dicts):
         play = True
         while play:
         
@@ -109,45 +131,78 @@ class MacGyver:
                 self._n_position = (self.position[0]+1, self.position[1]+0)
                 self.check_position(valid_cells)
                 self.check_objects(objects_dicts)
+                self.show(Cell.GRID_DIM, Cell.GRID_LIST,objects_dicts)
+                print(self)
                
             # move down
             elif arrow_getch == 224 and arrow_getche == 80:
                 self._n_position = (self.position[0]+0, self.position[1]+1)
                 self.check_position(valid_cells)
                 self.check_objects(objects_dicts)
+                self.show(Cell.GRID_DIM, Cell.GRID_LIST,objects_dicts)
+                print(self)
                                  
             # move left        
             elif arrow_getch == 224 and arrow_getche == 75:
                 self._n_position = (self.position[0]-1, self.position[1]+ 0)
                 self.check_position(valid_cells)
-                self.check_objects(objects_dicts)               
+                self.check_objects(objects_dicts)
+                self.show(Cell.GRID_DIM, Cell.GRID_LIST,objects_dicts)
+                print(self)               
                     
             # move up 
             elif arrow_getch == 224 and arrow_getche == 72:
                 self._n_position = (self.position[0]+0, self.position[1]-1)
                 self.check_position(valid_cells)
-                self.check_objects(objects_dicts)  
+                self.check_objects(objects_dicts)
+                self.show(Cell.GRID_DIM, Cell.GRID_LIST,objects_dicts)
+                print(self) 
                                
             else:
                 play = False
 
 
+    def __repr__(self):
+        return "MacGyver se trouve se trouve à la postion: {}."\
+        .format(self.position)
+
 class Guard: 
     
     def __init__(self):
-        self.position = (14,7) 
-        self.image = image
+        self.position = (0,0) 
+        self.image = "image"
+
+    def initial_position(self,valid_cells):
+        for key, value in valid_cells.items():
+            if value == "end":
+                self.position = key
+
 
 
 
 
 if __name__ == "__main__":
+    # Step 1: get source into this module
     content = a_id.get_source('labyrinthe.txt')
+
+    # Step 2: Generate my cell instances { position : (x,y), cell type : 'wall/path/start/end'} from source
     cell = Cell.initialize_cells(content)
-    valid_cells,valid_cells_list = Cell.available_paths(Cell.GRID)
+
+    # Step3 : create list and dict of 'valid' cells (where Mac can walk, wher objects can stand) 
+    valid_cells,valid_cells_list = Cell.available_paths(Cell.GRID_DICT)
+
+    # Step4 : Create objects  { position : (x,y), object name : 'Platic tube/Ether/Neddle'}
     Objects.initialize_objects(valid_cells_list)
-    mac = MacGyver((0,7))
-    MacGyver.mac_moves(mac, valid_cells, {(1, 7): 'Needle', (1, 4): 'Plastic tube', (11, 5): 'Ether'}
+
+    #Step 5 : Create MacGyver
+    # Option A
+    player = MacGyver()
+    player.initial_position(valid_cells)
+    print(player)
+
+
+
+    MacGyver.moves(player, valid_cells, {(1, 7): 'Needle', (1, 4): 'Plastic tube', (11, 5): 'Ether'}
 )
     # MacGyver.mac_moves(mac, valid_cells, Objects.OBJECTS_DICT)
     #print (nouv_pos)
