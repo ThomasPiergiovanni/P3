b9af348834b7b34096d6f8bfcbf64919cc1fc1c4
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-04-22 12:44:42 +0200

    Added functions description

--- principal.py
+++ principal.py
@@ -1,33 +1,36 @@
-import os
-import msvcrt
+import os             
+import msvcrt # to get keyboard arrow interaction
 import random
-import logging as lg
+import logging as lg # to get debug functionnalities
 import math
 
 import analysis.input_data as a_id # import source data
 
 lg.basicConfig(level=lg.INFO)
 
+# Class of cells composing the labyrinthe grid (i.e CELLS)
 class Cell:
     
     #class variable
 
-    GRID_DIM = 15
+    GRID_DIM = 15 #number of cells composing each side of the labyrinth grid (ie. labyrinthe grid is a square)
     CELLS=[]
     
     def __init__(self,xy_position,index_position,cell_type,):
         self.xy_position = xy_position    # (x, y) tuple
-        self.index_position = index_position    # index in list
-        self.cell_type = cell_type  # str
+        self.index_position = index_position    # index in CELLS. NB not sur I'll need it
+        self.cell_type = cell_type  # type of cell (ie: path, wall, objects, start , end, etc.)
 
+    # class method to generate cell instances that will be stored into CELLS list   
     @classmethod
-    def initialize_cells(cls,input_list):
+    def initialize_cells(cls,input_list): 
         for i,elt in enumerate(input_list):
             x = i % cls.GRID_DIM
             y = i // cls.GRID_DIM
             cell = Cell((x,y),i,elt)
             cls.CELLS.append(cell)
 
+# Class of objects to pick up in the labyrinthe grid (i.e CELLS)
 class Object: 
     
     OBJ = ["Needle","Plastic tube","Ether"]
@@ -40,8 +43,9 @@ class Object:
         self.object_name = name 
         self.image = "image"
     
+    # class method to generate objects intsances. Instances will be randomly positionned on a appropriate/valid cell
     @classmethod
-    def initialize_objects(cls,cells):
+    def initialize_objects(cls,cells): 
         for i,name in enumerate(cls.OBJ):
             valid_cells = [elt for elt in cells if elt.cell_type == "path"]
             rand_cell = random.choice(valid_cells)
@@ -51,18 +55,21 @@ class Object:
             objekt = Object(xy_position,index, cells_index,name)
             cls.OBJECTS.append(objekt)
 
+# Class of the Guard who restrict escape from the labyrinthe grid (i.e CELLS)
 class Guard: 
     
     def __init__(self):
         self.position = (0,0) 
         self.image = "image"
 
+    # method to set Guard position on the appropriate cell
     def initial_position(self,cells):
         xy_position = [elt.xy_position  for elt in cells if elt.cell_type == "end"]
         self.xy_position = xy_position[0]
-        index_position = [elt.index_position  for elt in cells if elt.cell_type == "end"]
-        self.index_position =index_position[0]
-            
+        index_position = [elt.index_position  for elt in cells if elt.cell_type == "end"] # index in CELLS. NB not sur I'll need it
+        self.index_position =index_position[0] 
+ 
+# Class of MacGyver. He will move on the lab, pick up objects and find the exit           
 class MacGyver: 
 
     
@@ -73,19 +80,18 @@ class MacGyver:
         self.image = "image"
         self.collected_objects =[]
 
-    
+    # method to set MacGyver position on the appropriate cell
     def initial_position(self,cells):
         xy_position = [elt.xy_position  for elt in cells if elt.cell_type == "start"]
         self.xy_position = xy_position[0]
-        index_position = [elt.index_position  for elt in cells if elt.cell_type == "start"]
+        index_position = [elt.index_position  for elt in cells if elt.cell_type == "start"] # index in CELLS. NB not sur I'll need it
         self.index_position =index_position[0]
 
-    def check_position(self,cells):
-
+    # Check if MacGyver new position is OK (not a wall), if he reached the end with all collected objects or not
+    def check_position(self,cells): 
         xy_position = [elt.xy_position for elt in cells if elt.xy_position == self._n_position and elt.cell_type != "wall" ]
         if xy_position:
             self.xy_position = xy_position[0]
-            play = True
             for elt in cells:
                 if self.xy_position == elt.xy_position and elt.cell_type == "end" and len(self.collected_objects) == 3:
                     print("YOU WIN!!!")
@@ -95,38 +101,39 @@ class MacGyver:
                     print("GAME OVER \nYou did not collect all objects!!!")
                     play = False
                     return play
-            return play
         play = True
         return play
-    def check_objects(self, objects):
 
+    # Check if MacGyver new position is where an object is. If yes he put it in his bag. 
+    def check_objects(self, objects):
         for i, elt in enumerate (objects):
             if elt.xy_position== self._n_position:
                 object_name = elt.object_name
                 self.collected_objects.append(object_name)
                 del objects[i]
-                print (len(self.collected_objects))
+
+    # Show the labyrinthe.
     def show (self,cells,objects):
-        _cells = []
+        _cells = [] # create a temporary grid for display display purpose
         for elt in cells:
-            if elt.xy_position == self.xy_position:
+            if elt.xy_position == self.xy_position: # if Mac is here , this cell will reflect that
                 _show_type = self.name
                 _show_xy = elt.xy_position
                 _show_index = elt.index_position
                 _cells.append([_show_type,_show_xy,_show_index])
-            else:
+            else:   # if Mac is not there , this cell will reflect the original grid
                 _show_type = elt.cell_type
                 _show_xy = elt.xy_position
                 _show_index = elt.index_position
                 _cells.append([_show_type,_show_xy,_show_index])
 
-        for _elt in _cells:
-            for obj in objects:
+        for _elt in _cells:     # The new temporary labyrinthe grid 
+            for obj in objects:     # if Object is here , this cell will reflect that
                 if _elt[1] == obj.xy_position:
                     _elt[0] = obj.object_name
 
-
-        for _elt in _cells:
+        # Change temporary labyrinthe grids value for siplay purposes (in terminal, one letter as dimmension 1:1. ok for grid display)
+        for _elt in _cells:  
             if _elt[0] == "start":
                 _elt [0] = "S" 
             elif _elt[0] == "end":
@@ -138,8 +145,9 @@ class MacGyver:
             elif _elt[0] == "MacGyver":
                 _elt [0] = "M"
             else:
-                _elt [0] = "O"
+                _elt [0] = "o"
 
+        # Create the labyrinthe view
         a = len(_cells)
         sq = int(math.sqrt(a))   
         for i,elt in enumerate(_cells):
@@ -154,14 +162,15 @@ class MacGyver:
                 labyrinthe_string += elt + " "
 
         print(labyrinthe_string)
-
         del _cells
     
+    # Initiate actions based on MacGyver moves
     def moves(self,cells,objects):
         play = True
         self.show(cells,objects)
         while play:
-        
+            
+            # get keyboard keys
             arrow_getch= ord(msvcrt.getch())
             arrow_getche = ord(msvcrt.getche())
             
@@ -202,28 +211,26 @@ class MacGyver:
                 play = False
 
 
-    def __repr__(self):
-        return "MacGyver se trouve se trouve à la postion: {}."\
-        .format(self.xy_position)
-
-
-
 if __name__ == "__main__":
+
     # Step 1: get source into this module
-    # content = a_id.get_source('labyrinthe.txt')
     content_ready = a_id.main('labyrinthe.txt')
 
-    # Step 2: Generate my cell instances { position : (x,y), cell type : 'wall/path/start/end'} from source
-
+    # Step 2: Generate cell instances
     Cell.initialize_cells(content_ready)
-    valid_cells = [elt.cell_type for elt in Cell.CELLS if elt.cell_type != "wall"]
 
-    # Step4 : Create objects  { position : (x,y), object name : 'Platic tube/Ether/Neddle'}
+    # Step3 : Generate objects instances
     Object.initialize_objects(Cell.CELLS)
-    # object_list= [(elt.xy_position,elt.object_name) for elt in Object.OBJECTS]
+
+    # Step4 : Generate guard instances
     guard = Guard()
     Guard.initial_position(guard,Cell.CELLS)
 
+    # Step5 : Generate macgyver instances
     player = MacGyver()
     MacGyver.initial_position(player,Cell.CELLS)
+
+    #Step6 : Control macgyver moves, create the wanted action and views.
     MacGyver.moves(player, Cell.CELLS, Object.OBJECTS)
+
+os.system("pause")
