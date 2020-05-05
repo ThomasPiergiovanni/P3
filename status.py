1831620040cf18c21405e62159f00d6072333247
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-05-05 16:18:30 +0200

    Refactoring: status module checked with pylint

--- status.py
+++ status.py
@@ -1,5 +1,9 @@
 #-*-coding:utf-8 -*
-class Status:   
+"""Status module
+"""
+class Status:
+    """Status class
+    """
     def __init__(self):
         self.main = True
         self.menu = True
@@ -7,16 +11,22 @@ class Status:
         self.game = 0
 
     def quit_game(self):
+        """Method sets booleans in order to quit main loop
+        """
         self.main = False
         self.menu = False
         self.play = False
 
     def play_game(self):
+        """Method sets booleans in order to start play loop
+        """
         self.main = True
         self.menu = False
         self.play = True
 
     def play_end(self):
+        """Method sets booleans in order to end play loop
+        """
         self.main = True
         self.menu = True
         self.play = False
