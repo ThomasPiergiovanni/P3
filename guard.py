8c8560c75d8026dc9ef4520bf51891caf616d7ae
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-05-05 17:14:38 +0200

    refactoring: guard module checked with pylint

--- guard.py
+++ guard.py
@@ -1,21 +1,26 @@
 #-*-coding:utf-8 -*
+"""Guard module
+"""
 import pygame
-import constants as constants
+import constants
 
-# Guard class
-class Guard:    
+class Guard:
+    """Guard class
+    """
     def __init__(self):
-        self.xy_position = (0,0) 
+        self.xy_position = (0, 0)
         self.image = pygame.image.load(constants.IMAGE_GUARD).convert_alpha()
-        
-    # method to set Guard position on the appropriate cell
-    def initial_position(self,grid_instance):
-        xy_position = [elt.xy_position  for elt in grid_instance.cells if\
-         elt.cell_type == 3]
-        self.xy_position = xy_position[0]  
-    
-    # method do display the guard   
-    def show(self,screen):
+
+    def initial_position(self, grid_instance):
+        """Method sets Guard instance on the appropriate cell.
+        """
+        xy_position = [elt.xy_position  for elt in grid_instance.cells if \
+        elt.cell_type == 3]
+        self.xy_position = xy_position[0]
+
+    def show(self, screen):
+        """Method for Guard instance display.
+        """
         x_display = self.xy_position[0] * constants.CELL_SIZE
         y_display = self.xy_position[1] * constants.CELL_SIZE
-        screen.blit(self.image,(x_display,y_display))
+        screen.blit(self.image, (x_display, y_display))
